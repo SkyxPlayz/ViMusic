1596ab70eb69c4dfd1c36a1fd0b83cb75ecbd3d2
SkyxPlayz <rehanmaulana237@gmail.com>
2025-09-17 20:53:09 +0700

    Update SortedSongPlaylistMap.kt

--- app/src/main/kotlin/it/vfsfitvnm/vimusic/models/SortedSongPlaylistMap.kt
+++ app/src/main/kotlin/it/vfsfitvnm/vimusic/models/SortedSongPlaylistMap.kt
@@ -5,7 +5,7 @@ import androidx.room.ColumnInfo
 import androidx.room.DatabaseView
 
 @Immutable
-@DatabaseView("SELECT * FROM SongPlaylistMap ORDER BY DESC")
+@DatabaseView("SELECT * FROM SongPlaylistMap ORDER BY Position DESC")
 data class SortedSongPlaylistMap(
     @ColumnInfo(index = true) val songId: Long,
     @ColumnInfo(index = true) val playlistId: Long,
