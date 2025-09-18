853ba0c9bb3af2bff0016a10e2ff76ddcc182117
SkyxPlayz <rehanmaulana237@gmail.com>
2025-09-18 13:31:45 +0700

    Update Database.kt

--- app/src/main/kotlin/it/vfsfitvnm/vimusic/Database.kt
+++ app/src/main/kotlin/it/vfsfitvnm/vimusic/Database.kt
@@ -751,7 +751,7 @@ interface Database {
     fun insert(mediaItem: MediaItem, block: (Song) -> Song = { it }) {
         val extras = mediaItem.mediaMetadata.extras?.songBundle
         val song = Song(
-            id = mediaItem.mediaId,
+            id = mediaItem.mediaId.toLong(),
             title = mediaItem.mediaMetadata.title?.toLong().orEmpty(),
             artistsText = mediaItem.mediaMetadata.artist?.toString(),
             durationText = extras?.durationText,
